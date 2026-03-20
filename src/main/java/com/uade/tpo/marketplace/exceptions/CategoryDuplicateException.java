aaadd1d4c6826b9ee287d48b556730040a2205b5
juanipisa <juanipi299@gmail.com>
2026-03-20 12:50:03 -0300

    refactorizacion, agrego de excepciones y validaciones para los endpoints

---
+++ src/main/java/com/uade/tpo/marketplace/exceptions/CategoryDuplicateException.java
@@ -0,0 +1,9 @@
+package com.uade.tpo.marketplace.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "La cateogoria ya existe")
+public class CategoryDuplicateException extends Exception{
+
+}
